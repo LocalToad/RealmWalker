7f83164b8d260c4c5d9c6366ebf8bf6fa4d3aafb
Aurora <Treibley95@gmail.com>
2026-08-14 18:04:04 -0400

    added scenes for the starting well and the starting town, remade the tut scene into the game scene, it will be the permanat scene that will call windows and chunks for the player to interact with from now on. edited the readme to have a section to list out know bugs that may or may not work but are being ignored

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RealmWalker
+
+#currently know bugs
+#1. 8-12-26 "player has the ability to phase through walls on the first 
+#turn of the game"
--- Scripts/tut.gd
+++ Scripts/tut.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 @export var player: PackedScene
 @export var slime: PackedScene
+@export var starting_well: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var starting_well = starting_well.instantiate()
+	add_child(starting_well)
 	var new_player = player.instantiate()
 	add_child(new_player)
 	new_player.global_position = Vector2(24,24)
