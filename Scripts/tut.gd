cb3230aed2d1da3cb78a30ce9349913ff7ee8a47
Aurora <Treibley95@gmail.com>
2026-08-14 21:29:33 -0400

    added the well and ladder sprites as well as added them to the maps, i have also added a _spawn() function that inputs the scene you want to spawn and the Vector2() coords of where you want the scene to spawn. This function is found within the game scene script

--- Scripts/tut.gd
+++ Scripts/tut.gd
@@ -4,14 +4,13 @@ extends Node2D
 @export var slime: PackedScene
 @export var starting_well: PackedScene
 
+func _spawn(scene, pos):
+	var iscene = scene.instantiate()
+	add_child(iscene)
+	iscene.position = pos
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var starting_well = starting_well.instantiate()
-	add_child(starting_well)
-	var new_player = player.instantiate()
-	add_child(new_player)
-	new_player.global_position = Vector2(24,24)
-	var slime1 = slime.instantiate()
-	add_child(slime1)
-	slime1.position = Vector2(104,184)
-	pass # Replace with function body.
+	_spawn(starting_well, Vector2(0,0))
+	_spawn(player, Vector2(24,24))
+	_spawn(slime, Vector2(136,136))
