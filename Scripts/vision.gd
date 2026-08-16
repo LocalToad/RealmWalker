778243fb1c977860802c2b008e2797f6475a6e63
OcularBlind <ocularblind@gmail.com>
2026-08-16 15:26:23 -0400

    another change for rebasing

--- Scripts/vision.gd
+++ Scripts/vision.gd
@@ -6,8 +6,12 @@ var _fog_tile_layer: TileMapLayer
 var x_dir = 1
 var y_dir = 1
 var temp_pos: Vector2
+var map_node = Node2D
 
 func _look(looker_pos_pixel_center: Vector2):
+	
+	#map_node = lib.startingWell.get_node("TileMap")
+	#print(str(map_node))
 	
 	if current_fog_node == null:
 		current_fog_node = lib._spawn(fog_map, Vector2(0,0))
@@ -24,16 +28,18 @@ func _look(looker_pos_pixel_center: Vector2):
 				
 				if _fog_tile_layer.get_cell_source_id(Vector2i(x, y)) ==0:
 					
-					if(x > looker_pos_pixel_center.x):
+					temp_pos = lib._pixel_center_to_tile(looker_pos_pixel_center)
+					
+					if(x > temp_pos.x):
 						x_dir = 1
 					else:
 						x_dir = -1
-					if(y > looker_pos_pixel_center.y):
+					if(y > temp_pos.y):
 						y_dir = 1
 					else:
 						y_dir = -1
 					
-					if(Vector2(x, y) == Vector2(0, 0)):
+					if(Vector2(x, y) == Vector2(0, 0) || Vector2(x, y) == Vector2(0, 2) || Vector2(x, y) == Vector2(2, 0) || Vector2(x, y) == Vector2(2, 2)):
 						print("testing tile:")
 						print("x = " + str(x))
 						print("y = " + str(y))
@@ -48,13 +54,37 @@ func _look(looker_pos_pixel_center: Vector2):
 						elif(x_dir == -1 and y_dir == -1):
 							print("tile is left and up from the player")
 					
-					var test_point = lib._tile_to_pixel_center(Vector2(x, y)) + Vector2(x_dir, y_dir) * (16 / 2)
+					var test_point = lib._tile_to_pixel_center(Vector2(x, y))
+					
+					if(x_dir == 1 and y_dir == 1):
+						test_point = test_point + Vector2(-8, -8)
+					elif(x_dir == -1 and y_dir == 1):
+						test_point = test_point + Vector2(8, -8)
+					elif(x_dir == 1 and y_dir == -1):
+						test_point = test_point + Vector2(-8, 8)
+					elif(x_dir == -1 and y_dir == -1):
+						test_point = test_point + Vector2(8, 8)
+						
+					#if(x_dir == 1 and y_dir == 1):
+						#test_point = test_point
+					#elif(x_dir == -1 and y_dir == 1):
+						#test_point = test_point
+					#elif(x_dir == 1 and y_dir == -1):
+						#test_point = test_point
+					#elif(x_dir == -1 and y_dir == -1):
+						#test_point = test_point
 					
 					var query = PhysicsRayQueryParameters2D.create(looker_pos_pixel_center, test_point)
 					var occlusion = space_state.intersect_ray(query)
+					
+					
 					if !occlusion || (occlusion.position - test_point).length() < 1:
 						_fog_tile_layer.erase_cell(Vector2i(x, y))
 						print("Clearing tile: " + str(Vector2i(x, y)))
+						print("the pixel we drew raycast to: ")
+						print("pixel: " + str(test_point))
+						print("the tile pixel we drew raycast from: ")
+						print("pixel: " + str(looker_pos_pixel_center))
 						
 				#if _fog_tile_layer.get_cell_source_id(Vector2i(x, y)) ==0:
 					#temp_pos.x = x
